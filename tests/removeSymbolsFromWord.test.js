3e9bc8856afe595d04ee743b35cce79c89e18902
nadika <nadikazavodovska@gmail.com>
2025-02-17 20:55:51 +0000

    Add tests to remove symbols before and after word

--- tests/removeSymbolsFromWord.test.js
+++ tests/removeSymbolsFromWord.test.js
@@ -1,11 +1,19 @@
 import { removeSymbolsFromWord } from "../script.js";
 
 describe("removeSymbolsFromWord function", () => {
-    test("should remove '!' before the word", () => {
-        expect(removeSymbolsFromWord("!hello")).toBe("hello");
+    test("should remove ',' before the word", () => {
+        expect(removeSymbolsFromWord(",world")).toBe("world");
     });
 
-    test("should remove '^' before the word", () => {
-        expect(removeSymbolsFromWord("^hello")).toBe("hello");
+    test("should remove '.' before the word", () => {
+        expect(removeSymbolsFromWord(".mouse")).toBe("mouse");
+    });
+
+    test("should remove '!' after the word", () => {
+        expect(removeSymbolsFromWord("hello!")).toBe("hello");
+    });
+
+    test("should remove '?' after the word", () => {
+        expect(removeSymbolsFromWord("fine?")).toBe("fine");
     });
 });
