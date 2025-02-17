b8efd195bc5e0921ad3dd4438b1fa15171935c08
nadika <nadikazavodovska@gmail.com>
2025-02-17 13:05:54 +0000

    Get input, btn

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ import words from "./words.json" with { type: "json" };
 let userDictionary = new Set();
 
 window.onload = function () {
-    
+    const userText = document.getElementById("userText");
+    console.log(userText);
+    const checkBtn = document.getElementById("checkBtn");
+    console.log(checkBtn);
 }
