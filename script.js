ed2504516c5243539e2b6bdabcace9faf7f86f67
nadika <nadikazavodovska@gmail.com>
2025-02-17 14:26:28 +0000

    Get, trim input text

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ import words from "./words.json" with { type: "json" };
 // User dictionary, which store words added by user without duplicates
 let userDictionary = new Set();
 
-window.onload = function () {
-    const userText = document.getElementById("userText"); 
+
+    const userTextInput = document.getElementById("userText"); 
     const checkBtn = document.getElementById("checkBtn"); 
     const mainBlock = document.getElementById("main-block");
 
     const checkResultBlock = document.createElement("div");
     checkResultBlock.id = "check-result-block";
     mainBlock.appendChild(checkResultBlock);
+
+checkBtn.addEventListener("click", checkSpelling);
+function checkSpelling(){
+    // Get value from the input and remove spaces before and after the text
+    const userText = userTextInput.value.trim();
+    // If after remove spaces userText is empty, function exits
+    if(!userText) {
+        return;
+    }
+    console.log(userText);
 }
