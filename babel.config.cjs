a226fece6e5839614c122b6e038175a98d93c6db
nadika <nadikazavodovska@gmail.com>
2025-02-19 18:51:40 +0000

    Add comments

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
     presets: [
+        // Used to transform modern JavaScript code to be compatible with the current version of Node.js
         ["@babel/preset-env", { targets: { node: "current" } }],
     ],
+    // transforms ES6 module syntax to commonJS syntax, enables babel to parse import assertions
     plugins: ["@babel/plugin-transform-modules-commonjs", "@babel/plugin-syntax-import-assertions"],
 };
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 import words from "./words.json" with { type: "json" };
 
-// User dictionary, which store words added by user without duplicates
+// User dictionary permanently stores words added manually by user without duplicates
 export let userDictionary = new Set();
 
 const checkResultBlock = document.createElement("div");
 checkResultBlock.id = "check-result-block";
+
 let userTextInput;
+
+// Temporary store words, which checkSpelling are flages as misspelled. Used only for checkSpelling function
 let userMisspelledWords;
 
 window.onload = function () {
@@ -19,11 +22,12 @@ window.onload = function () {
 };
 
 function checkSpelling() {
-    // Remove details about previous text
+    // Remove details about previous check
     checkResultBlock.textContent = "";
 
     // Get value (string) from the input and remove spaces before and after the text
     const userText = userTextInput.value.trim();
+
     // If after remove spaces userText is empty, alert and function exits
     if (!userText) {
         alert("You didn't type any text. Please, type your text.");
@@ -32,10 +36,12 @@ function checkSpelling() {
 
     // Convert userText string to array. Split by spaces.
     const userWordsArray = userText.split(/\s+/);
+
     userMisspelledWords = new Set();
 
     userWordsArray.forEach((word) => {
 
+        // Check if the first letter is capitalised
         if (word.match(/^[A-Z][a-z]*$/)) {
             return;
         }
@@ -45,18 +51,21 @@ function checkSpelling() {
             const wordPartsArray = word.split("-");
 
             wordPartsArray.forEach(oneWord => {
+                // Remove symbols from each word part, check if it's misspelled
                 const oneWordWithoutSymbols = removeSymbolsFromWord(oneWord);
                 if (!words.includes(oneWordWithoutSymbols) && !userDictionary.has(oneWordWithoutSymbols) && oneWord !== "") {
                     userMisspelledWords.add(oneWordWithoutSymbols);
                 }
             })
         } else {
+            // For regular words, remove symbols, check if it's misspelled
             const wordWithoutSymbols = removeSymbolsFromWord(word);
             if (!words.includes(wordWithoutSymbols) && !userDictionary.has(wordWithoutSymbols)) {
                 userMisspelledWords.add(wordWithoutSymbols);
             }
         }
     });
+    // If there are misspelled words, display them
     if (userMisspelledWords.size > 0) {
         displayCheckResultBlock([...userMisspelledWords]);
     } else {
@@ -70,10 +79,10 @@ export function removeSymbolsFromWord(word) {
 
 export function addToUserDictionary(word) {
     userDictionary.add(removeSymbolsFromWord(word));
-    checkResultBlock.classList.remove("misspelled-block-add-border");
     checkSpelling();
 }
 
+// display the misspelled words, add them to the dictionary
 function displayCheckResultBlock(words) {
     checkResultBlock.classList.add("misspelled-block-add-border");
     const misspelledWordsMessage = document.createElement("h2");
