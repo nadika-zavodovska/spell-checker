a4785db4eb78258ad8f99e391b0514acfda88c73
nadika <nadikazavodovska@gmail.com>
2025-02-19 13:24:53 +0000

    Fix logic with '-word.' like example

--- script.js
+++ script.js
@@ -35,20 +35,18 @@ function checkSpelling() {
     userMisspelledWords = new Set();
 
     userWordsArray.forEach((word) => {
+
         if (word.match(/^[A-Z]/)) {
             return;
         }
         if (word.includes("-")) {
+
             const wordPartsArray = word.split("-");
-            console.log(wordPartsArray);
-            // if (wordPartsArray.some(wordPart => !words.includes(wordPart) && !userDictionary.has(wordPart))) {
-            //     userMisspelledWords.add(word);
-            // }
+
             wordPartsArray.forEach(oneWord => {
-                console.log(wordPartsArray);
-                if (!words.includes(oneWord) && !userDictionary.has(oneWord) && oneWord !== "") {
-                    userMisspelledWords.add(oneWord);
-                    console.log(userMisspelledWords);
+                const oneWordWithoutSymbols = removeSymbolsFromWord(oneWord);
+                if (!words.includes(oneWordWithoutSymbols) && !userDictionary.has(oneWordWithoutSymbols) && oneWord !== "") {
+                    userMisspelledWords.add(oneWordWithoutSymbols);
                 }
             })
         } else {
@@ -61,7 +59,6 @@ function checkSpelling() {
     if (userMisspelledWords.size > 0) {
         displayCheckResultBlock([...userMisspelledWords]);
     }
-
 }
 
 export function removeSymbolsFromWord(word) {
